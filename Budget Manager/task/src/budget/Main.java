6f3ebc85d1b1b0da371a05af0d81bf2a53f2cf25
Denis Gikalo <denis.gikalo@outlook.com>
2025-01-27 03:51:45 +0500

    Start the project

---
+++ Budget Manager/task/src/budget/Main.java
@@ -0,0 +1,15 @@
+package budget;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            MainMenu.displayMainMenuOptions();
+
+            String option = SystemOperations.readData();
+
+
+        }
+    }
+}
