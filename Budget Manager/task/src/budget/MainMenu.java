b073c1b496c1c7bff389ddfa83b4aac3ca0856ca
Denis Gikalo <denis.gikalo@outlook.com>
2025-01-31 18:38:12 +0500

    Refactored Main Menu options text

--- Budget Manager/task/src/budget/MainMenu.java
+++ Budget Manager/task/src/budget/MainMenu.java
@@ -2,15 +2,14 @@ package budget;
 
 public class MainMenu {
     public static void displayMainMenuOptions() {
-        System.out.println(
-                """
+        String mainMenuOptions = """
                 Choose your action:
-                1) Add income;
+                1) Add income
                 2) Add purchase
                 3) Show list of purchases
                 4) Balance
-                0) Exit
-                """
-        );
+                0) Exit""";
+
+        SystemOperations.printData(mainMenuOptions);
     }
 }
