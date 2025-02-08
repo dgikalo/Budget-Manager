a4c5a4aa08f44a8131abedc734879b04e48c2a41
Denis Gikalo <denis.gikalo@outlook.com>
2025-02-08 22:40:12 +0500

    Build MainMenu class skeleton; The addIncomeOption, showBalanceOption and exitProgramOption methods were implemented

--- Budget Manager/task/src/budget/MainMenu.java
+++ Budget Manager/task/src/budget/MainMenu.java
@@ -1,4 +1,64 @@
 package budget;
 
-public class MainMenu {
+public final class MainMenu {
+    private MainMenu() {
+    }
+
+
+    public static void displayMainMenu() {
+        String mainMenuOptions = """
+                Choose your action:
+                1) Add income
+                2) Add purchase
+                3) Show list of purchases
+                4) Balance
+                0) Exit""";
+
+        System.out.println(mainMenuOptions);
+
+        int passedMainMenuOption = Integer.parseInt(SystemOperations.readInputData());
+
+        processMainMenuOption(passedMainMenuOption);
+    }
+
+
+    private static void processMainMenuOption(int option) {
+        switch (option) {
+            case 1 -> addIncomeOption();
+            case 2 -> addPurchaseOption();
+            case 3 -> showListOfPurchasesOption();
+            case 4 -> showBalanceOption();
+            case 0 -> exitProgramOption();
+        }
+    }
+
+
+    private static void addIncomeOption() {
+        float incomeValue = Float.parseFloat(SystemOperations.readInputData());
+
+        Balance.updateBalance(incomeValue);
+
+        System.out.println("Income was added!");
+    }
+
+
+    private static void addPurchaseOption() {
+
+    }
+
+
+    private static void showListOfPurchasesOption() {
+
+    }
+
+
+    private static void showBalanceOption() {
+        System.out.printf("Balance: $%.2f\n", Balance.getBalance());
+    }
+
+
+    private static void exitProgramOption() {
+        System.out.println("Bye!");
+        System.exit(0);
+    }
 }
