d3436269d3734b7677039c827df3cb9ec3274d8d
Denis Gikalo <denis.gikalo@outlook.com>
2025-02-08 22:26:40 +0500

    Minor fixes

--- Budget Manager/task/src/budget/Balance.java
+++ Budget Manager/task/src/budget/Balance.java
@@ -3,9 +3,11 @@ package budget;
 public class Balance {
     private static float balance = 0;
 
+
     public static float getBalance() {
         return balance;
     }
+
 
     public static void updateBalance(float value) {
         Balance.balance += value;
