339ccd1b4de300596ac9ba9bf41bd8657996ab4a
Denis Gikalo <denis.gikalo@outlook.com>
2025-02-21 23:47:11 +0300

    The Main class was reimplemented

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -3,13 +3,8 @@ package budget;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        while (true) {
-            MainMenu.displayMainMenuOptions();
-
-            String option = SystemOperations.readData();
-
-
-        }
+        MainMenuHandler.runMainMenu();
     }
 }
