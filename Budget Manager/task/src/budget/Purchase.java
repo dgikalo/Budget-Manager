e3c2dcb6858fbb39b84552c118622281f55f647e
Denis Gikalo <denis.gikalo@outlook.com>
2025-03-11 20:10:59 +0300

    Change attribute type in method signature

--- Budget Manager/task/src/budget/Purchase.java
+++ Budget Manager/task/src/budget/Purchase.java
@@ -1,29 +1,4 @@
 package budget;
 
-public class Purchase {
-    private final PurchaseType purchaseType;
-    private final String purchaseName;
-    private final float purchaseCost;
-
-
-    public Purchase(PurchaseType type, String name, float cost) {
-        this.purchaseType = type;
-        this.purchaseName = name;
-        this.purchaseCost = cost;
-    }
-
-
-    public PurchaseType getPurchaseType() {
-        return purchaseType;
-    }
-
-
-    public String getPurchaseName() {
-        return purchaseName;
-    }
-
-
-    public float getPurchaseCost() {
-        return purchaseCost;
-    }
+public record Purchase(Category category, String name, float price) {
 }
