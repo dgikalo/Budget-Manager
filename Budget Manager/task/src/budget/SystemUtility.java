0015d1b6e24a518d43b6c936aed690f3c6c4b883
Denis Gikalo <denis.gikalo@outlook.com>
2025-03-11 20:25:05 +0300

    The PrintError methos removed; Other minor fixes

--- Budget Manager/task/src/budget/SystemUtility.java
+++ Budget Manager/task/src/budget/SystemUtility.java
@@ -1,16 +1,13 @@
 package budget;
 
+
 import java.util.Scanner;
+
 
 public class SystemUtility {
 
     public static String readData() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
-    }
-
-
-    static void printError() {
-        System.out.println("The purchase list is empty!");
     }
 }
