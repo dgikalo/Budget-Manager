c8bd22649caf7937ca6ba39a278c688e5526670e
Denis Gikalo <denis.gikalo@outlook.com>
2025-03-25 01:07:50 +0300

    - The SelectionContext class implemented.

--- Budget Manager/task/src/budget/SelectionContext.java
+++ Budget Manager/task/src/budget/SelectionContext.java
@@ -1,4 +1,26 @@
 package budget;
 
+
+import java.util.List;
+import java.util.Map;
+
+
 public class SelectionContext {
+
+    private SortSelectionAlgorithm algorithm;
+
+
+    public void setAlgorithm(SortSelectionAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+
+    public List<String> sortPurchasesList(List<Purchase> purchasesList) {
+        return algorithm.getSortedList(purchasesList);
+    }
+
+
+    public List<String> sortPurchasesList(Map<Type, Float> totalSumByType) {
+        return algorithm.getSortedList(totalSumByType);
+    }
 }
